10e0659fbb65d74a93baae838a2d2ed8467039aa
LI-YONG-QI <a2369028@gmail.com>
2022-02-13 21:48:52 +0800

    mint and get name on attribute is success

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Web3 from 'web3'
 import './App.css';
 import {Navbar, Container, Button, Alert, Row, Col, Card} from 'react-bootstrap'; 
@@ -7,7 +7,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   const [account, setAccount] = useState('null');
+  const [token, setToken] = useState(null)
   const [Quantity, setQuantity] = useState(0);
+
+  useEffect(async () =>{
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(prev => accounts[0])
+
+    window.web3 = new Web3(window.ethereum)
+    const web3 = window.web3
+    const networkId = await web3.eth.net.getId();
+    const networkData = Character.networks[networkId]
+
+    if(networkData){
+      const abi = Character.abi
+      const address = networkData.address
+      setToken(prev => new web3.eth.Contract(abi, address))
+    }
+    
+  }, [])
+
   const cardArray = [
     {
       name: 'zero',
@@ -30,33 +49,7 @@ const App = () => {
     return Boolean(ethereum && ethereum.isMetaMask);
   };
 
-  const connectAccount = async () => {
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    setAccount(accounts[0])
-    console.log("connectAccount" + accounts[0])
-    window.web3 = new Web3(window.ethereum)
-    const web3 = window.web3
-    const networkId = await web3.eth.net.getId();
-    const networkData = Character.networks[networkId]
-
-    if(networkData){
-      const abi = Character.abi
-      const address = networkData.address
-      const token = new web3.eth.Contract(abi, address)
-
-      //mint attr
-      /*await token.methods.mint(0, "Hat", "equip", window.location.origin + '/images/zero.png').send({from: accounts[0]})
-      .then(console.log)*/
- 
-      //attach Hat to tokenId=0
-      await token.methods.name(0).call().then(console.log)
-      /*const temp = await token.methods.balanceOf(0, 0).call()
-      console.log(temp)
-
-      setQuantity(async () => await token.methods.balanceOf(0, 0).call())*/
-  }
-}
-
+  
   const nav = () =>{
     return (
       <Navbar bg="dark" variant="dark">
@@ -68,6 +61,15 @@ const App = () => {
     )
   }
 
+  const mintAttr = async () => {
+    await token.methods.mint(0, "Hat", "equip", window.location.origin + "hat").send({from: account})
+  }
+
+  const getName = async () =>{
+    const name = await token.methods.name(0).call()
+    setQuantity(prev => name)
+  }
+
   const isConnect = () => {
     console.log("isConnect" + account)
     if(account != 'null'){
@@ -77,8 +79,7 @@ const App = () => {
       return(
         <Button 
           className="d-flex" 
-          variant='outline-light' 
-          onClick={connectAccount}>
+          variant='outline-light' >
         Connect Wallect
         </Button>
       )
@@ -103,7 +104,8 @@ const App = () => {
                   src={imageGenerator()}/>
               </Col>
               <Col><Button variant='outline-success'>組合</Button></Col>
-              <Col><Button variant='outline-danger'>拆分</Button></Col>
+              <Col><Button variant='outline-danger'onClick={getName}>disp</Button></Col>
+              <Col><Button variant='outline-warning' onClick={mintAttr}>鑄造attrToken</Button></Col>
               <Col>{Quantity}</Col>
             </Row>
           </Container>
